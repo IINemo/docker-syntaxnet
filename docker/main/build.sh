d7ec733f7136d932f0c1b68202e83c4df59ca84f
IINemo <artemshelmanov@gmail.com>
2017-04-28 19:04:22 +0300

    Fixed the building scripts.

--- docker/main/build.sh
+++ docker/main/build.sh
@@ -4,11 +4,12 @@ MODEL_URL="$3"
 
 
 SCRIPT_DIR=$(dirname $0)
-rsync -avz --links --delete --exclude ".svn" "$SCRIPT_DIR/../syntaxnet_api_server/src/syntaxnet_api_server/" "$SCRIPT_DIR/api/" || exit -1
+rsync -avz --links --delete --exclude ".svn" "$SCRIPT_DIR/../../syntaxnet_api_server/src/syntaxnet_api_server/" "$SCRIPT_DIR/api/" || exit -1
 
 cat Dockerfile.tmpl > Dockerfile
 sed -i "s/<%PLACEHOLDER_LANG_LOCALE%>/$LANG_LOCALE/g" Dockerfile
-sed -i "s/<%PLACEHOLDER_MODEL_URL%>/$MODEL_URL/g" Dockerfile
+sed -i "s|<%PLACEHOLDER_MODEL_URL%>|$MODEL_URL|g" Dockerfile
+sed -i "s|<%PLACEHOLDER_MODEL_NAME%>|$(basename $MODEL_URL)|g" Dockerfile
 
 docker build -t "$NAME" $SCRIPT_DIR
 RES=$?
